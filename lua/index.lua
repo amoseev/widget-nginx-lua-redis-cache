cfcfc7ba1e2f8324b2cf8cdc3afac55a4b9f2e0e
artem moseev <am@yclients.com>
2017-09-25 10:36:13 +0300

    Update index.lua

--- lua/index.lua
+++ lua/index.lua
@@ -1,49 +1,6 @@
 local CONFIG_REDIS_HOST = "127.0.0.1"
 local CONFIG_REDIS_PORT = 6379
 
-
-function vd_string(o)
-    return '"' .. tostring(o) .. '"'
-end
-
-
-function vd_recurse(o, indent)
-    if indent == nil then indent = '' end
-    local indent2 = indent .. '  '
-    if type(o) == 'table' then
-        local s = indent .. '{' .. '\n'
-        local first = true
-        for k, v in pairs(o) do
-            if first == false then s = s .. ', \n' end
-            if type(k) ~= 'number' then k = vd_string(k) end
-            s = s .. indent2 .. '[' .. k .. '] = ' .. vd_recurse(v, indent2)
-            first = false
-        end
-        return s .. '\n' .. indent .. '}'
-    else
-        return vd_string(o)
-    end
-end
-
-function var_dump_cli(...)
-    local args = { ... }
-    if #args > 1 then
-        var_dump_cli(args)
-    else
-        print(vd_recurse(args[1]))
-    end
-end
-
-function var_dump(...)
-    local args = { ... }
-    if #args > 1 then
-        var_dump(args)
-    else
-        ngx.say(vd_recurse(args[1]))
-    end
-end
-
-
 local function failRedisFetch()
     ngx.status = 404
     ngx.print("Not nound")
